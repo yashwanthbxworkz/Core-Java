b2b70f4524a890386d3c1ae61e5a685794143db5
yashwanthbxworkz <yashwantha.xwork@gmail.com>
2024-07-05 23:52:18 +0530

    basics of methodes

---
+++ Projector.java
@@ -0,0 +1,10 @@
+class Projector{
+	public static  void display(){
+		System.out.println("projectorcn use to display some informetion");
+	System.out.println("projector method is started");
+	//String nameOfBrand = "XGIMI";
+	//float rating = 5.0f;
+	//System.out.println("projector method is ended");
+	return;
+	}
+}
---
+++ ProjectorTester.java
@@ -0,0 +1,8 @@
+class ProjectorTester{
+		public static void main(String[] messages){
+		System.out.println("main stated");
+		Projector.display();
+		System.out.println("main ended");
+		
+		}
+		}
